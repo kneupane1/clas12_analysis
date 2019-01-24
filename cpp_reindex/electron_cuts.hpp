7126772170ba2fb54bc95c21178b72d8a7cd652a
Krishna Neupane <krishna_physics@yahoo.com>
2019-01-24 13:05:42 -0500

    updated

--- cpp_reindex/electron_cuts.hpp
+++
@@ -1,9 +0,0 @@
-
-#ifndef ELECTRON_CUTS_H_GUARD
-#define ELECTRON_CUTS_H_GUARD
-#include "constants.hpp"
-#include <iostream>
-
-electron_cuts(int ec, int cc, int dc, float sc, float sf, int charge) {}
-
-#endif
--- cpp_reindex/eletron_cuts.cpp
+++
@@ -1,1 +0,0 @@
-#include "electron_cuts.hpp"
