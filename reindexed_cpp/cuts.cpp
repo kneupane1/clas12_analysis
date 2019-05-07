cbb39c61d6db63f15c32fc2ab36b4718104909eb
Krishna Neupane <krishna_physics@yahoo.com>
2019-05-07 11:07:10 -0400

    updates

--- reindexed_cpp/cuts.cpp
+++ reindexed_cpp/cuts.cpp
@@ -2,26 +2,28 @@
 #include "cuts.hpp"
 using namespace std;
 Cuts::Cuts() {
-  _status = std::nan("-99");
-  _charge = std::nan("-99");
-  _min_mom = std::nan("-99");
-  _sf = std::nan("-99");
-  _vertex_pos = std::nan("-99");
+        _status = std::nan("-99");
+        _charge = std::nan("-99");
+        _min_mom = std::nan("-99");
+        _sf = std::nan("-99");
+        _vertex_pos = std::nan("-99");
 
-  _good_e = false;
+        _good_e = false;
 }
-Cuts::~Cuts() {}
+Cuts::~Cuts() {
+}
 bool Cuts::electron_cuts(int status, int charge, float min_mom, float sf, float vertex_pos) {
-  if (2000 < status < 4000) {
-    if (charge == -1) {
-      if (min_mom > 1.0) {
-        if (sf > 0.15 && sf < 0.28) {
-          if (-10.0 < vertex_pos < 5.0) {
-            _good_e = true;
-          }
+
+        if (2000 < status < 4000) {
+                if (charge == -1) {
+                        if (min_mom > 1.0) {
+                                if (sf > 0.18 && sf < 0.28) {
+                                        if (-10.0 < vertex_pos < 5.0) {
+                                                _good_e = true;
+                                        }
+                                }
+                        }
+                }
         }
-      }
-    }
-  }
-  return _good_e;
+        return _good_e;
 }
