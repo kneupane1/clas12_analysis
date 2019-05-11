5f56ed9d6ce67413c46c27d74838c17dc260dcc2
Krishna Neupane <krishna_physics@yahoo.com>
2019-05-11 09:40:57 -0400

    updates

--- reindexed_cpp/main.hpp
+++ reindexed_cpp/main.hpp
@@ -148,7 +148,7 @@ void datahandeler(std::string fin, std::string fout) {
           hist->Fill_deltat_pip(pid->at(part), charge->at(part), dt->dt_Pi(), p->at(part));
           hist->Fill_deltat_kp(pid->at(part), charge->at(part), dt->dt_K(), p->at(part));
           //    if (charge->at(part) == 1) {
-          if ((abs(dt->dt_P()) < 0.5) || (dt->dt_P() > -4.5 && dt->dt_P() < -3.7) && (pid->at(part) == 2112)) {
+          if ((abs(dt->dt_P()) < 0.5) || (dt->dt_P() > -4.5 && dt->dt_P() < -3.7) && (pid->at(part) == 2212)) {
             event->SetProton(px->at(part), py->at(part), pz->at(part), MASS_P);
             good_p = e_cuts->proton_cuts(status->at(part), charge->at(part), event->p_mu_prime().P(), pid->at(part),
                                          chi2pid->at(part));
@@ -214,197 +214,194 @@ void datahandeler(std::string fin, std::string fout) {
         if (event->p_mu_prime_cm().Theta() > 0) {
           hist->Fill_theta_P(event->p_mu_prime_cm().Theta() * (180 / PI), event->pip_mu_prime_cm().Theta() * (180 / PI),
                              event->pim_mu_prime_cm().Theta() * (180 / PI));
-=======
-event->SetPim(px->at(part), py->at(part), pz->at(part), MASS_PIP);
-good_pim =
-    e_cuts->pim_cuts(status->at(part), charge->at(part), event->pim_mu_prime().P(), pid->at(part), chi2pid->at(part));
-if (pid->at(part) == PIM) {
-  cc_tot_pim = cc_nphe_tot->at(part);
-  if (cc_tot_pim >= 0) {
-    hist->Fill_hist_cc_tot_pim(cc_tot_pim);
-  }
-  cc_ltcc_pim = cc_ltcc_nphe->at(part);
-  if (cc_ltcc_pim >= 0) {
-    hist->Fill_hist_cc_ltcc_pim(cc_ltcc_pim);
-  }
-  cc_htcc_pim = cc_htcc_nphe->at(part);
-  if (cc_htcc_pim >= 0) {
-    hist->Fill_hist_cc_htcc_pim(cc_htcc_pim);
-  }
-}
-}
-}
-else if (charge->at(part) == 1) {
-  hist->Fill_deltat_prot(pid->at(part), charge->at(part), dt->dt_P(), p->at(part));
-  hist->Fill_deltat_pip(pid->at(part), charge->at(part), dt->dt_Pi(), p->at(part));
-  hist->Fill_deltat_kp(pid->at(part), charge->at(part), dt->dt_K(), p->at(part));
-  //    if (charge->at(part) == 1) {
-  if ((abs(dt->dt_P()) < 0.5) || (dt->dt_P() > -4.5 && dt->dt_P() < -3.7)) {
-    event->SetProton(px->at(part), py->at(part), pz->at(part), MASS_P);
-    good_p = e_cuts->proton_cuts(status->at(part), charge->at(part), event->p_mu_prime().P(), pid->at(part),
-                                 chi2pid->at(part));
-  } else if ((abs(dt->dt_Pi()) < 0.50) ||
+          event->SetPim(px->at(part), py->at(part), pz->at(part), MASS_PIP);
+          good_pim = e_cuts->pim_cuts(status->at(part), charge->at(part), event->pim_mu_prime().P(), pid->at(part),
+                                      chi2pid->at(part));
+          if (pid->at(part) == PIM) {
+            cc_tot_pim = cc_nphe_tot->at(part);
+            if (cc_tot_pim >= 0) {
+              hist->Fill_hist_cc_tot_pim(cc_tot_pim);
+            }
+            cc_ltcc_pim = cc_ltcc_nphe->at(part);
+            if (cc_ltcc_pim >= 0) {
+              hist->Fill_hist_cc_ltcc_pim(cc_ltcc_pim);
+            }
+            cc_htcc_pim = cc_htcc_nphe->at(part);
+            if (cc_htcc_pim >= 0) {
+              hist->Fill_hist_cc_htcc_pim(cc_htcc_pim);
+            }
+          }
+        }
+      } else if (charge->at(part) == 1) {
+        hist->Fill_deltat_prot(pid->at(part), charge->at(part), dt->dt_P(), p->at(part));
+        hist->Fill_deltat_pip(pid->at(part), charge->at(part), dt->dt_Pi(), p->at(part));
+        hist->Fill_deltat_kp(pid->at(part), charge->at(part), dt->dt_K(), p->at(part));
+        //    if (charge->at(part) == 1) {
+        if ((abs(dt->dt_P()) < 0.5) || (dt->dt_P() > -4.5 && dt->dt_P() < -3.7)) {
+          event->SetProton(px->at(part), py->at(part), pz->at(part), MASS_P);
+          good_p = e_cuts->proton_cuts(status->at(part), charge->at(part), event->p_mu_prime().P(), pid->at(part),
+                                       chi2pid->at(part));
+        } else if ((abs(dt->dt_Pi()) < 0.50) ||
                                                    (dt->dt_Pi() > -4.25 && dt->dt_P() < -3.7) /*&&
                                                                                                               event->pip_mu_prime().P() < 1.2)*/) {
-    event->SetPip(px->at(part), py->at(part), pz->at(part), MASS_PIP);
-    good_pip = e_cuts->pip_cuts(status->at(part), charge->at(part), event->pip_mu_prime().P(), pid->at(part),
-                                chi2pid->at(part));
-    // if (pid->at(part) == 2212 && charge->at(part) > 0) {
-    if (pid->at(part) == PIP) {
-      cc_tot_pip = cc_nphe_tot->at(part);
-      if (cc_tot_pip >= 0) {
-        hist->Fill_hist_cc_tot_pip(cc_tot_pip);
-      }
-      cc_ltcc_pip = cc_ltcc_nphe->at(part);
-      if (cc_ltcc_pip >= 0) {
-        hist->Fill_hist_cc_ltcc_pip(cc_ltcc_pip);
-      }
-      cc_htcc_pip = cc_htcc_nphe->at(part);
-      if (cc_htcc_pip >= 0) {
-        hist->Fill_hist_cc_htcc_pip(cc_htcc_pip);
-      }
-    }
-  }
-}
-else event->SetOther(px->at(part), py->at(part), pz->at(part), MASS_N, pid->at(part));
-}
-}
-if (good_pip == false) continue;
-if (good_p == false) continue;
-if (good_pim == false) continue;
-
-// dt->dt_calc_1(p->at(part), sc_ctof_time->at(part), sc_ctof_path->at(part));
-// if (sc_ctof_component->at(part) > 0) {
-//         //  std::cout << "dt_ctof" << dt->dt_ctof_P() - dt->dt_P() << "    "
-//  << sc_ctof_component->at(part) << '\n';
-
-hist->Fill_sf_vs_lu((ec_ecin_lu->at(0) + ec_ecout_lu->at(0) + ec_pcal_lu->at(0)) / 3,
-                    (ec_tot_energy->at(0) / event->e_mu_prime().P()));
-hist->Fill_sf_vs_lv((ec_ecin_lv->at(0) + ec_ecout_lv->at(0) + ec_pcal_lv->at(0)) / 3,
-                    (ec_tot_energy->at(0) / event->e_mu_prime().P()));
-hist->Fill_sf_vs_lw((ec_ecin_lw->at(0) + ec_ecout_lw->at(0) + ec_pcal_lw->at(0)) / 3,
-                    (ec_tot_energy->at(0) / event->e_mu_prime().P()));
-hist->Fill_lu_dist((ec_ecin_lu->at(0) + ec_ecout_lu->at(0) + ec_pcal_lu->at(0)) / 3);
-hist->Fill_lv_dist((ec_ecin_lv->at(0) + ec_ecout_lv->at(0) + ec_pcal_lv->at(0)) / 3);
-hist->Fill_lw_dist((ec_ecin_lw->at(0) + ec_ecout_lw->at(0) + ec_pcal_lw->at(0)) / 3);
-
-hist->Fill_vertex_vz(vz->at(0));
-
-// if (event->twoPionEvent()) {
-
-//  for (int i = 1; i < sector; i++) {
-// if (event->p_mu_prime().P() != 0) {
-hist->Fill_WvsQ2(event->W(), event->Q2(), sector);
-//}
-//}
-delete dt;
-if (/*event->W() < 1.40 &&  event->W() > 1.20 &&*/ event->Q2() < 15.0 && event->Q2() > 0.0) {
-  event->CalcMissMass();
-  event->AlphaCalc();
-  if (event->twoPionEvent()) {
-    if (event->p_mu_prime_cm().Theta() > 0) {
-      hist->Fill_theta_P(event->p_mu_prime_cm().Theta() * (180 / PI), event->pip_mu_prime_cm().Theta() * (180 / PI),
-                         event->pim_mu_prime_cm().Theta() * (180 / PI));
+          event->SetPip(px->at(part), py->at(part), pz->at(part), MASS_PIP);
+          good_pip = e_cuts->pip_cuts(status->at(part), charge->at(part), event->pip_mu_prime().P(), pid->at(part),
+                                      chi2pid->at(part));
+          // if (pid->at(part) == 2212 && charge->at(part) > 0) {
+          if (pid->at(part) == PIP) {
+            cc_tot_pip = cc_nphe_tot->at(part);
+            if (cc_tot_pip >= 0) {
+              hist->Fill_hist_cc_tot_pip(cc_tot_pip);
+            }
+            cc_ltcc_pip = cc_ltcc_nphe->at(part);
+            if (cc_ltcc_pip >= 0) {
+              hist->Fill_hist_cc_ltcc_pip(cc_ltcc_pip);
+            }
+            cc_htcc_pip = cc_htcc_nphe->at(part);
+            if (cc_htcc_pip >= 0) {
+              hist->Fill_hist_cc_htcc_pip(cc_htcc_pip);
+            }
+          }
+        }
+      } else
+        event->SetOther(px->at(part), py->at(part), pz->at(part), MASS_N, pid->at(part));
     }
 
-    hist->Fill_Phi_cm(event->p_mu_prime_cm().Phi() * (180 / PI), event->pip_mu_prime_cm().Phi() * (180 / PI),
-                      event->pim_mu_prime_cm().Phi() * (180 / PI));
+    if (good_pip == false) continue;
+    if (good_p == false) continue;
+    if (good_pim == false) continue;
 
-    //{
-    // std::cout << "electron " << event->q_cm().M() << '\n';
+    // dt->dt_calc_1(p->at(part), sc_ctof_time->at(part), sc_ctof_path->at(part));
+    // if (sc_ctof_component->at(part) > 0) {
+    //         //  std::cout << "dt_ctof" << dt->dt_ctof_P() - dt->dt_P() << "    "
+    //  << sc_ctof_component->at(part) << '\n';
 
-    // n += 1;
-    // std::cout
-    //<< "  pip_pim_alpha_cm  " << event->alpha_ppip_pipim()
-    //<< "  pip_theta_cm  "
-    //<< event->pip_mu_prime_cm().Theta() * (180 / PI)
-    //<< "   pim_theta_cm  "
-    //  << event->pim_mu_prime_cm().Theta() * (180 / PI) << "  " << n
-    //  << '\n';
-  }
-  if (event->elecProtEvent()) {
-    hist->Fill_ep_mm(event->MM(), sector);
-    hist->Fill_ep_mmSQ(event->MM2(), sector);
-    if (event->MM2() < 0.1 && event->MM2() > -0.1) {
-      hist->Fill_WvsmmSQ_ep(event->W_ep(), event->MM2(), sector);
-    } else {
-      hist->Fill_WvsmmSQ_anti_ep(event->W_ep(), event->MM2(), sector);
-    }
+    hist->Fill_sf_vs_lu((ec_ecin_lu->at(0) + ec_ecout_lu->at(0) + ec_pcal_lu->at(0)) / 3,
+                        (ec_tot_energy->at(0) / event->e_mu_prime().P()));
+    hist->Fill_sf_vs_lv((ec_ecin_lv->at(0) + ec_ecout_lv->at(0) + ec_pcal_lv->at(0)) / 3,
+                        (ec_tot_energy->at(0) / event->e_mu_prime().P()));
+    hist->Fill_sf_vs_lw((ec_ecin_lw->at(0) + ec_ecout_lw->at(0) + ec_pcal_lw->at(0)) / 3,
+                        (ec_tot_energy->at(0) / event->e_mu_prime().P()));
+    hist->Fill_lu_dist((ec_ecin_lu->at(0) + ec_ecout_lu->at(0) + ec_pcal_lu->at(0)) / 3);
+    hist->Fill_lv_dist((ec_ecin_lv->at(0) + ec_ecout_lv->at(0) + ec_pcal_lv->at(0)) / 3);
+    hist->Fill_lw_dist((ec_ecin_lw->at(0) + ec_ecout_lw->at(0) + ec_pcal_lw->at(0)) / 3);
 
-  } else if (event->twoPionEvent()) {
-    hist->Fill_2pion_mm(event->MM(), sector);
-    hist->Fill_2pion_mmSQ(event->MM2(), sector);
-    if (event->MM2() < 0.1 && event->MM2() > -0.1) {
-      hist->Fill_WvsmmSQ_2pi(event->W_2pi(), event->W_delta_pp(), event->W_delta_zero(), event->W_rho(), event->MM2(),
-                             sector);
-    } else {
-      hist->Fill_WvsmmSQ_anti_2pi(event->W_2pi(), event->W_delta_pp(), event->W_delta_zero(), event->W_rho(),
-                                  event->MM2(), sector);
-    }
-    //  std::cout << "w_2pi   " << event->W_2pi() << '\n';
+    hist->Fill_vertex_vz(vz->at(0));
 
-    // std::cout << "diff " << event->W_2pi() - event->W() << '\n';
-  } else if (event->ProtonPimEvent()) {
-    hist->Fill_pip_mm(event->MM(), sector);
-    hist->Fill_pip_mmSQ(event->MM2(), sector);
+    // if (event->twoPionEvent()) {
 
-  } else if (event->ProtonPipEvent()) {
-    hist->Fill_pim_mm(event->MM(), sector);
-    hist->Fill_pim_mmSQ(event->MM2(), sector);
-  }
-
-  event->CalcMissMass_wop();
-
-  if (event->elecWopEvent()) {
-    hist->Fill_MM_wop_e_prime(event->MM_wop(), sector);
-    hist->Fill_MMSQ_wop_e_prime(event->MM2_wop(), sector);
-  } else if (event->twoPionWopEvent()) {
-    hist->Fill_MM_wop_2pion(event->MM_wop(), sector);
-    hist->Fill_MMSQ_wop_2pion(event->MM2_wop(), sector);
-
-  } else if (event->WopPimEvent()) {
-    hist->Fill_MM_wop_pip(event->MM_wop(), sector);
-    hist->Fill_MMSQ_wop_pip(event->MM2_wop(), sector);
-  } else if (event->WopPipEvent()) {
-    hist->Fill_MM_wop_pim(event->MM_wop(), sector);
-    hist->Fill_MMSQ_wop_pim(event->MM2_wop(), sector);
-    if (event->MM2_wop() < 1.05 && event->MM2_wop() > 0.7) {
-      hist->Fill_WvsmmSQ_singlepip(event->W_singlepip(), event->MM2_wop(), sector);
-    } else {
-      hist->Fill_WvsmmSQ_anti_singlepip(event->W_singlepip(), event->MM2_wop(), sector);
-    }
-  }
-}
-delete e_cuts;
-delete event;
->>>>>>> b7677dc757f2ac927e69665971bc730557a76473
+    //  for (int i = 1; i < sector; i++) {
+    // if (event->p_mu_prime().P() != 0) {
+    hist->Fill_WvsQ2(event->W(), event->Q2(), sector);
+    //}
+    //}
+    delete dt;
+    if (/*event->W() < 1.40 &&  event->W() > 1.20 &&*/ event->Q2() < 15.0 && event->Q2() > 0.0) {
+      event->CalcMissMass();
+      event->AlphaCalc();
+      if (event->twoPionEvent()) {
+        if (event->p_mu_prime_cm().Theta() > 0) {
+          hist->Fill_theta_P(event->p_mu_prime_cm().Theta() * (180 / PI), event->pip_mu_prime_cm().Theta() * (180 / PI),
+                             event->pim_mu_prime_cm().Theta() * (180 / PI));
         }
 
-        out->cd();
-        TDirectory *CC_EC = out->mkdir("ccEc");
-        CC_EC->cd();
-        hist->Write_EC();
-        hist->Write_hist_cc();
+        hist->Fill_Phi_cm(event->p_mu_prime_cm().Phi() * (180 / PI), event->pip_mu_prime_cm().Phi() * (180 / PI),
+                          event->pim_mu_prime_cm().Phi() * (180 / PI));
 
-        TDirectory *MM = out->mkdir("missingMass");
-        MM->cd();
-        hist->Write_MM_hist();
+        //{
+        // std::cout << "electron " << event->q_cm().M() << '\n';
 
-        TDirectory *wvsq2 = out->mkdir("wvsq2");
-        wvsq2->cd();
-        hist->Write_WvsQ2();
-
-        TDirectory *mom_vs_beta = out->mkdir("mom_vs_beta");
-        mom_vs_beta->cd();
-        hist->Write_MomVsBeta();
-
-        TDirectory *deltat_ftof = out->mkdir("deltat_ftof");
-        deltat_ftof->cd();
-        hist->Write_deltat();
-
-        out->Close();
-        chain->Reset();
-        std::cerr << "\nErrors: " << total << "\t" << std::endl;
-        delete hist;
+        // n += 1;
+        // std::cout
+        //<< "  pip_pim_alpha_cm  " << event->alpha_ppip_pipim()
+        //<< "  pip_theta_cm  "
+        //<< event->pip_mu_prime_cm().Theta() * (180 / PI)
+        //<< "   pim_theta_cm  "
+        //  << event->pim_mu_prime_cm().Theta() * (180 / PI) << "  " << n
+        //  << '\n';
       }
+      if (event->elecProtEvent()) {
+        hist->Fill_ep_mm(event->MM(), sector);
+        hist->Fill_ep_mmSQ(event->MM2(), sector);
+        if (event->MM2() < 0.1 && event->MM2() > -0.1) {
+          hist->Fill_WvsmmSQ_ep(event->W_ep(), event->MM2(), sector);
+        } else {
+          hist->Fill_WvsmmSQ_anti_ep(event->W_ep(), event->MM2(), sector);
+        }
+
+      } else if (event->twoPionEvent()) {
+        hist->Fill_2pion_mm(event->MM(), sector);
+        hist->Fill_2pion_mmSQ(event->MM2(), sector);
+        if (event->MM2() < 0.1 && event->MM2() > -0.1) {
+          hist->Fill_WvsmmSQ_2pi(event->W_2pi(), event->W_delta_pp(), event->W_delta_zero(), event->W_rho(),
+                                 event->MM2(), sector);
+        } else {
+          hist->Fill_WvsmmSQ_anti_2pi(event->W_2pi(), event->W_delta_pp(), event->W_delta_zero(), event->W_rho(),
+                                      event->MM2(), sector);
+        }
+        //  std::cout << "w_2pi   " << event->W_2pi() << '\n';
+
+        // std::cout << "diff " << event->W_2pi() - event->W() << '\n';
+      } else if (event->ProtonPimEvent()) {
+        hist->Fill_pip_mm(event->MM(), sector);
+        hist->Fill_pip_mmSQ(event->MM2(), sector);
+
+      } else if (event->ProtonPipEvent()) {
+        hist->Fill_pim_mm(event->MM(), sector);
+        hist->Fill_pim_mmSQ(event->MM2(), sector);
+      }
+
+      event->CalcMissMass_wop();
+
+      if (event->elecWopEvent()) {
+        hist->Fill_MM_wop_e_prime(event->MM_wop(), sector);
+        hist->Fill_MMSQ_wop_e_prime(event->MM2_wop(), sector);
+      } else if (event->twoPionWopEvent()) {
+        hist->Fill_MM_wop_2pion(event->MM_wop(), sector);
+        hist->Fill_MMSQ_wop_2pion(event->MM2_wop(), sector);
+
+      } else if (event->WopPimEvent()) {
+        hist->Fill_MM_wop_pip(event->MM_wop(), sector);
+        hist->Fill_MMSQ_wop_pip(event->MM2_wop(), sector);
+      } else if (event->WopPipEvent()) {
+        hist->Fill_MM_wop_pim(event->MM_wop(), sector);
+        hist->Fill_MMSQ_wop_pim(event->MM2_wop(), sector);
+        if (event->MM2_wop() < 1.05 && event->MM2_wop() > 0.7) {
+          hist->Fill_WvsmmSQ_singlepip(event->W_singlepip(), event->MM2_wop(), sector);
+        } else {
+          hist->Fill_WvsmmSQ_anti_singlepip(event->W_singlepip(), event->MM2_wop(), sector);
+        }
+      }
+    }
+    delete e_cuts;
+    delete event;
+  }
+
+  out->cd();
+  TDirectory *CC_EC = out->mkdir("ccEc");
+  CC_EC->cd();
+  hist->Write_EC();
+  hist->Write_hist_cc();
+
+  TDirectory *MM = out->mkdir("missingMass");
+  MM->cd();
+  hist->Write_MM_hist();
+
+  TDirectory *wvsq2 = out->mkdir("wvsq2");
+  wvsq2->cd();
+  hist->Write_WvsQ2();
+
+  TDirectory *mom_vs_beta = out->mkdir("mom_vs_beta");
+  mom_vs_beta->cd();
+  hist->Write_MomVsBeta();
+
+  TDirectory *deltat_ftof = out->mkdir("deltat_ftof");
+  deltat_ftof->cd();
+  hist->Write_deltat();
+
+  out->Close();
+  chain->Reset();
+  std::cerr << "\nErrors: " << total << "\t" << std::endl;
+  delete hist;
+}
 #endif
